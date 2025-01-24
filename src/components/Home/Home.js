39ad14ffca81381aeef203030543187ad67865fc
Densingh-123 <ddensingh19@gmail.com>
2025-01-24 23:05:05 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -35,7 +35,7 @@
                   src={homeLogo}
                   alt="home pic"
                   className="img-fluids"
-                  style={{ maxHeight: "560px", width: "240", objectFit: "cover" }}
+                  style={{ maxHeight: "540px", width: "240", objectFit: "cover" }}
                 />
               </Col>
             </Row>
